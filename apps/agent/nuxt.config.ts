6c6464e314675ee4de4a94abf2c7e812b1333e7c
shota-kamezawa <75684759+shota-kamezawa@users.noreply.github.com>
2024-07-30 19:55:11 +0900

    feat: use project references

--- apps/agent/nuxt.config.ts
+++ apps/agent/nuxt.config.ts
@@ -14,10 +14,6 @@ export default defineNuxtConfig({
           '@packages/utils/*': ['../../../packages/utils/*'],
         },
       },
-      include: [
-        '../../../packages/ui/**/*',
-        '../../../packages/utils/**/*',
-      ],
     },
   },
 })
